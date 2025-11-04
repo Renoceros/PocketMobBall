e432e75becaf2c1e20c973df011f4495e7e574c9
Renoceros <morenokristophan@gmail.com>
2025-11-05 03:53:43 +0700

    textures+changelog

--- src/main/java/com/pocketmobball/item/PomoballItem.java
+++ src/main/java/com/pocketmobball/item/PomoballItem.java
@@ -1,13 +1,14 @@
 package com.pocketmobball.item;
 
-import com.pocketmobball.registry.ModEntities;
-import com.pocketmobball.entity.PomoballEntity;
+// import com.pocketmobball.registry.ModEntities;
+// import com.pocketmobball.entity.PomoballEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.thrown.SnowballEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
+// import net.minecraft.sound.SoundCategory;
+// import net.minecraft.sound.SoundEvents;
+// import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -19,21 +20,15 @@ public class PomoballItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack stack = user.getStackInHand(hand);
-
-        world.playSound(null, user.getX(), user.getY(), user.getZ(),
-                SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL,
-                0.5F, 0.4F / (world.getRandom().nextFloat() * 0.4F + 0.8F));
-
+        ItemStack itemStack = user.getStackInHand(hand);
         if (!world.isClient) {
-            PomoballEntity entity = new PomoballEntity(ModEntities.POMOBALL_ENTITY_TYPE, world);
-            entity.setItem(stack);
-            entity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
-            world.spawnEntity(entity);
+            SnowballEntity snowball = new SnowballEntity(world, user);
+            snowball.setItem(itemStack);
+            snowball.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
+            world.spawnEntity(snowball);
         }
-
-        user.incrementStat(Stats.USED.getOrCreateStat(this));
-        if (!user.getAbilities().creativeMode) stack.decrement(1);
-        return TypedActionResult.success(stack, world.isClient());
+        user.getItemCooldownManager().set(this, 20); // 1s cooldown
+        return TypedActionResult.success(itemStack, world.isClient());
     }
+
 }
