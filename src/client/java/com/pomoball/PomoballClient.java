8cabd887d42f7eec9ff9c642e8b0c6dd465406be
Renoceros <morenokristophan@gmail.com>
2025-11-04 19:37:18 +0700

    Init

---
+++ src/client/java/com/pomoball/PomoballClient.java
@@ -0,0 +1,10 @@
+package com.pomoball;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PomoballClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
